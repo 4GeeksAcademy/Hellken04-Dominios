51338fabf56c92f9bca2132d19220e556101a919
Hellken04 <elkin_rondon@hotmail.com>
2025-04-22 02:23:17 +0000

    SE cambiaron los else if por la funcion domainEnd

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-let pronouns = ['the', 'our'];
-let adjs = ['great', 'big'];
-let nouns = ['jogger', 'racoon'];
-let domains =[ ".com", ".net", ".us",".io",".co",".on",".ger"];
 
+function domainEnd(noun,dom){
+    if (noun.slice(-3) == dom.slice(-3)) {
+        return noun.slice(0, -3) + dom;
+    }
+    if (noun.slice(-2) == dom.slice(-2)) {
+        return noun.slice(0, -2) + dom;
+    }
+    return noun + dom;
+}
 
-console.log("Estos son los dominios encontrados");
-for(let pronoun of pronouns){
-    for(let adj of adjs){
-        for(let noun of nouns){
-            for(let dom of domains){
-                if(noun.slice(-3)==dom.slice(-3)){
-                    console.log(pronoun+adj+noun.slice(0,-3)+dom);
-                }else if (noun.slice(-2)==dom.slice(-2)){
-                    console.log(pronoun+adj+noun.slice(0,-2)+dom);
-                }
-                else {
-                    console.log(pronoun+adj+noun+dom);
+    let pronouns = ['the', 'our'];
+    let adjs = ['great', 'big'];
+    let nouns = ['jogger', 'racoon'];
+    let domains = [".com", ".net", ".us", ".io", ".co", ".on", ".ger"];
+    console.log("Estos son los dominios encontrados");
+    for (let pronoun of pronouns) {
+        for (let adj of adjs) {
+            for (let noun of nouns) {
+                for (let dom of domains) {
+                    let domain = pronoun + adj + domainEnd(noun,dom);
+                    console.log(domain);
                 }
             }
         }
     }
-}
+
+
+
+
